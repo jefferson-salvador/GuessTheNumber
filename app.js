eb052a4e5c09f485577184881333810df7bd3c70
Jefferson Salvador <jefferson.behionsalvador@gmail.com>
2020-10-29 08:03:55 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var num1 = Math.floor(Math.random() * 10);
+const guessNum = () => {
+    var guess = document.getElementById("guess").value;
+    
+    if(guess > num1){
+        document.getElementById("result").innerHTML = `The number is lower than ${guess} <br> Try again`;
+    }
+    else if(guess < num1){
+        document.getElementById("result").innerHTML = `The number is greater than ${guess} <br> Try again`;
+    }
+    else{
+        document.getElementById("result").innerHTML = `Congratulations! The number is ${guess}`;
+    }
+    logs(guess);
+    document.getElementById('guess').value = '';
+}
+
+const numArray = [];
+const logs =(guess)=>{
+    numArray.push(guess);
+    document.getElementById("logs").innerHTML = `${numArray}`;
+}
